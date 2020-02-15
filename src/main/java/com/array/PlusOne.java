27ba8f7a71095604a37bde78cd2f313ffc7ffbe0
stayform <1092605759@qq.com>
2020-02-15 12:55:16 +0800

    加一

---
+++ src/main/java/com/array/PlusOne.java
@@ -0,0 +1,19 @@
+package com.array;
+//加一
+/**
+ * AuThor：StAY_
+ * Create:2020/2/15
+ */
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--){
+            digits[i]++;
+            digits[i] %=10;
+            if(digits[i]!=0)//表示无进位
+                return digits;
+        }
+        int[] digit = new int[digits.length+1];
+        digit[0]=1;
+        return digit;
+    }
+}
