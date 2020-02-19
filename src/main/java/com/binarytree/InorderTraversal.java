d053275f03096309b13b0b5a001b06387af2883d
stayform <1092605759@qq.com>
2020-02-19 22:10:07 +0800

    二叉树中序遍历

---
+++ src/main/java/com/binarytree/InorderTraversal.java
@@ -0,0 +1,40 @@
+package com.binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * AuThor：StAY_
+ * Create:2020/2/19
+ */
+public class InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        while(root!=null||!stack.isEmpty()){
+            while(root!=null){
+                stack.add(root);
+                root=root.left;
+            }
+            root=stack.pop();
+            list.add(root.val);
+            root=root.right;
+
+        }
+        return list;
+    }
+
+    /**
+     List<Integer> list = new ArrayList<>();
+     public List<Integer> inorderTraversal(TreeNode root) {
+     if(root==null){
+     return list;
+     }
+     if(root.left!=null)inorderTraversal(root.left);
+     list.add(root.val);
+     if(root.right!=null)inorderTraversal(root.right);
+     return list;
+     }
+     */
+}
