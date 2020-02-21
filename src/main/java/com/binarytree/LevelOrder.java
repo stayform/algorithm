52f56eba6bc94d349ab3ee114938eb6e3ae3b6da
stayform <1092605759@qq.com>
2020-02-21 12:44:37 +0800

    二叉树层序遍历

---
+++ src/main/java/com/binarytree/LevelOrder.java
@@ -0,0 +1,38 @@
+package com.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * AuThor：StAY_
+ * Create:2020/2/21
+ */
+//二叉树层序遍历
+public class LevelOrder {
+    List<List<Integer>> list = new ArrayList<>();
+    Queue<TreeNode> queue = new LinkedList<>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root==null){
+            return list;
+        }
+        queue.offer(root);
+
+
+        while(!queue.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();//size不能写在for循环中因为循环里执行的语句会让size产生变化
+            for(int i=0;i<size;i++){
+                TreeNode node =queue.poll();
+                level.add(node.val);
+                if(node.left!=null) queue.offer(node.left);
+                if(node.right!=null) queue.offer(node.right);
+            }
+            list.add(level);
+        }
+
+
+        return list;
+    }
+}
